434cb6dd7f2daeb604708fa29d3d4bb21aae0d9a
Oliver Dettenborn <oliver.dettenborn@nubank.com.br>
2026-04-29 21:35:41 -0300

    fix(ci): use tcp check in wait-on and 127.0.0.1 for better Windows reliability

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8081",
+    baseUrl: "http://127.0.0.1:8081",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
